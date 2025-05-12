7043947538bfcc18148b586ac0d9a5b7b0757da2
suttermorris <sutter_morris@brown.edu>
2025-05-12 19:01:31 -0400

    Update app_conf.py

--- demo/backend/server/app_conf.py
+++ demo/backend/server/app_conf.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 APP_ROOT = os.getenv("APP_ROOT", "/opt/sam2")
 
-API_URL = os.getenv("API_URL", "http://54.162.205.212:7263")
+API_URL = os.getenv("API_URL", "http://52.203.38.26:7263")
 
 MODEL_SIZE = os.getenv("MODEL_SIZE", "base_plus")
 
